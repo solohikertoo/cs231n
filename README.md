8589e1ad3b6933b7051b0694c02c19f388adfe50
solohikertoo <solohikertoo@gmail.com>
2020-07-05 16:22:40 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cs231n
 assignments and project for stanford cs231n deep learning computer vision course
+
+requirements for the environment (output of 'conda list') is in the file 'conda_list.txt'
+
+completed notebooks and python code are in folders:
+assignment1
+assignment2
+assignment3
+project
+
+
---
+++ project/image_utils.py
@@ -0,0 +1,106 @@
+
+import os
+import tempfile
+
+import numpy as np
+from imageio import imread
+from PIL import Image
+
+import torchvision.transforms as T
+
+
+"""
+Utility functions used for viewing and processing images.
+"""
+
+SQUEEZENET_MEAN = np.array([0.485, 0.456, 0.406], dtype=np.float32)
+SQUEEZENET_STD = np.array([0.229, 0.224, 0.225], dtype=np.float32)
+
+def preprocess_image(img):
+    """Preprocess an image for squeezenet.
+    
+    Subtracts the pixel mean and divides by the standard deviation.
+    """
+    return (img.astype(np.float32)/255.0 - SQUEEZENET_MEAN) / SQUEEZENET_STD
+
+
+def deprocess_image(img, rescale=False):
+    """Undo preprocessing on an image and convert back to uint8."""
+    img = (img * SQUEEZENET_STD + SQUEEZENET_MEAN)
+    if rescale:
+        vmin, vmax = img.min(), img.max()
+        img = (img - vmin) / (vmax - vmin)
+    return np.clip(255 * img, 0.0, 255.0).astype(np.uint8)
+
+
+def load_image(filename, size=None):
+    """Load and resize an image from disk.
+
+    Inputs:
+    - filename: path to file
+    - size: size of shortest dimension after rescaling
+    """
+    img = np.array(imread(filename))
+    if size is not None:
+        orig_shape = np.array(img.shape[:2])
+        min_idx = np.argmin(orig_shape)
+        scale_factor = float(size) / orig_shape[min_idx]
+        new_shape = (orig_shape * scale_factor).astype(int)
+        img = np.array(Image.fromarray(img).resize(size=(new_shape[1],new_shape[0])))
+    return img
+    
+def preprocess(img, size=512):
+    transform = T.Compose([
+        T.Resize(size),
+        T.ToTensor(),
+        T.Normalize(mean=SQUEEZENET_MEAN.tolist(),
+                    std=SQUEEZENET_STD.tolist()),
+        T.Lambda(lambda x: x[None]),
+    ])
+    return transform(img)
+
+def deprocess(img):
+    transform = T.Compose([
+        T.Lambda(lambda x: x[0]),
+        T.Normalize(mean=[0, 0, 0], std=[1.0 / s for s in SQUEEZENET_STD.tolist()]),
+        T.Normalize(mean=[-m for m in SQUEEZENET_MEAN.tolist()], std=[1, 1, 1]),
+        T.Lambda(rescale),
+        T.ToPILImage(),
+    ])
+    return transform(img)
+
+def rescale(x):
+    low, high = x.min(), x.max()
+    x_rescaled = (x - low) / (high - low)
+    return x_rescaled
+
+# takes an image, a model (cnn), and returns a list of
+# feature maps, one per layer.
+def extract_features(x, cnn):
+    """
+    Use the CNN to extract features from the input image x.
+    
+    Inputs:
+    - x: A PyTorch Tensor of shape (N, C, H, W) holding a minibatch of images that
+      will be fed to the CNN.
+    - cnn: A PyTorch model that we will use to extract features.
+    
+    Returns:
+    - features: A list of feature for the input images x extracted using the cnn model.
+      features[i] is a PyTorch Tensor of shape (N, C_i, H_i, W_i); recall that features
+      from different layers of the network may have different numbers of channels (C_i) and
+      spatial dimensions (H_i, W_i).
+    """
+    features = []
+    prev_feat = x
+    for i, module in enumerate(cnn._modules.values()):
+        next_feat = module(prev_feat)
+        features.append(next_feat)
+        prev_feat = next_feat
+    return features
+
+def features_from_img(imgpath, imgsize):
+    img = preprocess(PIL.Image.open(imgpath), size=imgsize)
+    img_var = img.type(dtype)
+    return extract_features(img_var, cnn), img_var
+
